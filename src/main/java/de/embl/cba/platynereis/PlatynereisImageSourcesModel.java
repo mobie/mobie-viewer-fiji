abebb89778109962195dd9360c5c6909589d1945
Christian Tischer <christian.tischer@embl.de>
2019-04-10 22:30:13 +0200

    WIP Multiple label sources

--- src/main/java/de/embl/cba/platynereis/PlatynereisImageSourcesModel.java
+++ src/main/java/de/embl/cba/platynereis/PlatynereisImageSourcesModel.java
@@ -20,25 +20,19 @@ public class PlatynereisImageSourcesModel implements ImageSourcesModel
 	public static final String EM_RAW_FILE_ID = "em-raw-";
 	public static final String tablesFolder = "tables";
 
-	private final Map< String, SourceAndMetadata< ? > > nameToSourceAndMetadata;
+	private Map< String, SourceAndMetadata< ? > > imageIdToSourceAndMetadata;
 	private final File dataFolder;
 
 	public PlatynereisImageSourcesModel( File dataFolder )
 	{
 		this.dataFolder = dataFolder;
-
-		nameToSourceAndMetadata = new HashMap<>();
-
-		List< File > imageFiles = getFiles( dataFolder, ".*.xml" );
-		for ( File imageFile : imageFiles )
-			addSource( imageFile );
-
+		addSources();
 	}
 
 	@Override
 	public Map< String, SourceAndMetadata< ? > > sources()
 	{
-		return nameToSourceAndMetadata;
+		return imageIdToSourceAndMetadata;
 	}
 
 	@Override
@@ -47,36 +41,54 @@ public class PlatynereisImageSourcesModel implements ImageSourcesModel
 		return false;
 	}
 
-	private SourceMetadata metadataFromSpimData( File imageSourceFile )
+
+	private void addSources()
 	{
-		final String imageId = sourceName( imageSourceFile );
+		imageIdToSourceAndMetadata = new HashMap<>();
+		List< File > imageFiles = getFiles( dataFolder, ".*.xml" );
+		for ( File imageFile : imageFiles )
+			addSource( imageFile );
+	}
+
+	private SourceMetadata getMetadata( File imageSourceFile )
+	{
+		final String imageId = imageId( imageSourceFile );
 		final SourceMetadata metadata = new SourceMetadata( imageId );
-		metadata.displayRangeMin = 0.0D;
-		metadata.displayRangeMax = 1000.0D;
 		metadata.numSpatialDimensions = 3;
 		metadata.displayName = imageId;
+		setDisplayRange( imageId, metadata );
+		setFlavour( imageId, metadata );
+		return metadata;
+	}
 
-		if ( imageId.contains( EM_RAW_FILE_ID ) )
-		{
-			metadata.displayRangeMin = 0.0D;
-			metadata.displayRangeMax = 255.0D;
-		}
-
+	private void setFlavour( String imageId, SourceMetadata metadata )
+	{
 		if ( imageId.contains( LABELS_FILE_ID ) )
 		{
 			metadata.flavour = SourceMetadata.Flavour.LabelSource;
 
-			final File tableFile = new File( getTablePath( imageId ) );
-
-			if ( tableFile.exists() )
-				metadata.segmentsTable = tableFile;
+			final File table = new File( getTablePath( imageId ) );
+			if ( table.exists() )
+				metadata.segmentsTable = table;
 		}
 		else
 		{
 			metadata.flavour = SourceMetadata.Flavour.IntensitySource;
 		}
+	}
 
-		return metadata;
+	private void setDisplayRange( String imageId, SourceMetadata metadata )
+	{
+		if ( imageId.contains( EM_RAW_FILE_ID ) )
+		{
+			metadata.displayRangeMin = 0.0D;
+			metadata.displayRangeMax = 255.0D;
+		}
+		else
+		{
+			metadata.displayRangeMin = 0.0D;
+			metadata.displayRangeMax = 1000.0D;
+		}
 	}
 
 	private String getTablePath( String sourceName )
@@ -84,7 +96,7 @@ public class PlatynereisImageSourcesModel implements ImageSourcesModel
 		return dataFolder + File.separator + tablesFolder + File.separator + sourceName + ".csv";
 	}
 
-	private static String sourceName( File file )
+	private static String imageId( File file )
 	{
 		String dataSourceName = file.getName().replaceAll( BDV_XML_SUFFIX, "" );
 
@@ -113,16 +125,12 @@ public class PlatynereisImageSourcesModel implements ImageSourcesModel
 		}
 	}
 
-	public void addSource( File file )
+	private void addSource( File file )
 	{
-		final String imageId = sourceName( file );
-
+		final String imageId = imageId( file );
 		final LazySpimSource lazySpimSource = new LazySpimSource( imageId, file );
-
-		final SourceMetadata metadata = metadataFromSpimData( file );
-
-		nameToSourceAndMetadata.put( imageId, new SourceAndMetadata( lazySpimSource, metadata ) );
+		final SourceMetadata metadata = getMetadata( file );
+		imageIdToSourceAndMetadata.put( imageId, new SourceAndMetadata( lazySpimSource, metadata ) );
 	}
-
 
 }
