395b8681056debbeb214f2f346b411f66a1d846f
Christian Tischer <christian.tischer@embl.de>
2019-05-27 15:17:33 +0200

    WIP: Save bdv as voxel image

--- src/test/java/users/pao/ExportRegisteredFibSemLabels.java
+++ src/test/java/users/pao/ExportRegisteredFibSemLabels.java
@@ -5,8 +5,13 @@ import bdv.util.BdvFunctions;
 import bdv.util.BdvOptions;
 import bdv.viewer.Interpolation;
 import de.embl.cba.bdv.utils.BdvUtils;
+import de.embl.cba.bdv.utils.io.BdvRaiVolumeExport;
+import de.embl.cba.bdv.utils.io.BdvRaiXYZCTExport;
 import de.embl.cba.platynereis.utils.Utils;
 import de.embl.cba.transforms.utils.Transforms;
+import ij.ImagePlus;
+import ij.io.FileSaver;
+import ij.measure.Calibration;
 import itc.utilities.CopyUtils;
 import itc.utilities.VectorUtils;
 import mpicbg.spim.data.SpimData;
@@ -24,6 +29,7 @@ import net.imglib2.realtransform.Scale3D;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.util.Intervals;
+import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 import org.renjin.gnur.api.S;
 
@@ -42,16 +48,18 @@ public class ExportRegisteredFibSemLabels
 				.getImgLoader().getSetupImgLoader( setupId )
 				.getImage( 0 );
 
+		final String voxelUnit = "micrometer";
 
 		final double[] targetVoxelSpacing = getTargetVoxelSpacing( targetSpimData, setupId );
-		Utils.log( "Target voxel spacing [unit]: " + VectorUtils.toString( targetVoxelSpacing ) );
+		Utils.log( "Target voxel spacing [" + voxelUnit + "]: " + VectorUtils.toString( targetVoxelSpacing ) );
+
 
 		Utils.log( "REMOVE: Target interval [voxel]: " + IntervalUtils.toString( targetInterval )  );
 
 		final RealInterval targetRealInterval
 				= IntervalUtils.toCalibratedRealInterval( targetInterval, targetVoxelSpacing );
 
-		Utils.log( "Target interval [unit]: " + IntervalUtils.toString( targetRealInterval ) );
+		Utils.log( "Target interval [" + voxelUnit + "]: " + IntervalUtils.toString( targetRealInterval ) );
 		
 		final SpimData spimData = new XmlIoSpimData().load(
 				"/Volumes/arendt/EM_6dpf_segmentation/EM-Prospr/em-raw-parapod-fib-affine_g.xml");
@@ -62,13 +70,13 @@ public class ExportRegisteredFibSemLabels
 		
 		final double[] sourceVoxelSpacing = BdvUtils.getVoxelSpacings( spimData, 0 ).get( level );
 
-		Utils.log( "Source voxel spacing [unit]: " + VectorUtils.toString( sourceVoxelSpacing ) );
+		Utils.log( "Source voxel spacing [" + voxelUnit + "]: " + VectorUtils.toString( sourceVoxelSpacing ) );
 
 		//double[] scalingFactors = createScalingFactors( targetVoxelSpacing, sourceVoxelSpacing );
 
 		final double[] scalingFactors = Arrays.stream( targetVoxelSpacing ).map( x -> 1.0 / x ).toArray();
 
-		Utils.log( "Voxel spacing scaling factors: " + VectorUtils.toString( scalingFactors ) );
+		// Utils.log( "Voxel spacing scaling factors: " + VectorUtils.toString( scalingFactors ) );
 
 		final Scale3D scale3D = new Scale3D( scalingFactors );
 
@@ -92,16 +100,30 @@ public class ExportRegisteredFibSemLabels
 		final RandomAccessibleInterval< T > transformedRasteredInterval =
 				Views.interval( transformedRastered, transformedSourceInterval );
 
-		final RandomAccessibleInterval< T > slice =
-				Views.hyperSlice( transformedRasteredInterval, 2, (int)( 152 * scalingFactors[ 2 ] ) );
+//		final RandomAccessibleInterval< T > slice =
+//				Views.hyperSlice( transformedRasteredInterval, 2, (int)( 152 * scalingFactors[ 2 ] ) );
 
+		Utils.log( "Creating target image..." );
 		final RandomAccessibleInterval< T > copy =
-				CopyUtils.copyPlanarRaiMultiThreaded( slice, 4 );
+				CopyUtils.copyVolumeRaiMultiThreaded( transformedRasteredInterval, 4 );
 
-		BdvFunctions.show( copy, "", BdvOptions.options().is2D() ).setDisplayRange( 0, 255 );
+		Utils.log( "Saving as Tiff..." );
+		final ImagePlus imagePlus = asImagePlus( copy, "", voxelUnit, targetVoxelSpacing );
+		new FileSaver( imagePlus ).saveAsTiff( "/Users/tischer/Desktop/test.tif" );
 
+		Utils.log( "Saving as Bdv..." );
+		final RandomAccessibleInterval< T > raiXYZCT =
+				Views.addDimension(
+						Views.addDimension( copy, 0, 0 ),
+						0, 0 );
 
-
+		new BdvRaiXYZCTExport< T >().export(
+				raiXYZCT,
+				voxelUnit,
+				"/Users/tischer/Desktop/test",
+				targetVoxelSpacing,
+				voxelUnit,
+				new double[]{0,0,0});
 
 //
 //		final Interval interval = Intervals.largestContainedInterval( realInterval );
@@ -123,6 +145,27 @@ public class ExportRegisteredFibSemLabels
 //
 //		transformed.add( Views.interval( transformedRA, transformedInterval ) );
 
+	}
+
+	public static ImagePlus asImagePlus( RandomAccessibleInterval raiXYZ,
+										 String title,
+										 String voxelUnit,
+										 double[] voxelSpacing )
+	{
+		final ImagePlus imp = ImageJFunctions.wrap(
+				Views.permute(
+						Views.addDimension( raiXYZ, 0, 0 ),
+						2, 3 ), title );
+
+		final Calibration calibration = new Calibration();
+		calibration.pixelWidth = voxelSpacing[ 0 ];
+		calibration.pixelHeight = voxelSpacing[ 1 ];
+		calibration.pixelDepth = voxelSpacing[ 2 ];
+		calibration.setUnit( voxelUnit );
+
+		imp.setCalibration( calibration );
+
+		return imp;
 	}
 
 	public static double[] getTargetVoxelSpacing( SpimData medData, int setupId )
