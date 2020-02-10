d8b843f1eebb3bbc7f75fa08ca7ac1f153a45bf3
Christian Tischer <christian.tischer@embl.de>
2020-02-10 11:33:11 +0100

    Add shell script to copy bdv-core and n5 jars to Fiji.app/jars

---
+++ src/test/resources/copy-jars-to-fiji.sh
@@ -0,0 +1,9 @@
+# bdv
+cp /Users/tischer/.m2/repository/sc/fiji/bigdataviewer-core/8.0.1-SNAPSHOT/bigdataviewer-core-8.0.1-SNAPSHOT.jar /Applications/Fiji.app/jars/
+
+# n5
+cp /Users/tischer/.m2/repository/org/janelia/saalfeldlab/n5/2.0.2/n5-2.0.2.jar /Applications/Fiji.app/jars/
+cp /Users/tischer/.m2/repository/org/janelia/saalfeldlab/n5-imglib2/3.4.1/n5-imglib2-3.4.1.jar /Applications/Fiji.app/jars/
+cp /Users/tischer/.m2/repository/org/janelia/saalfeldlab/n5-aws-s3/2.1.1-SNAPSHOT/n5-aws-s3-2.1.1-SNAPSHOT.jar /Applications/Fiji.app/jars/
+cp /Users/tischer/.m2/repository/com/amazonaws/aws-java-sdk-s3/1.11.534/aws-java-sdk-s3-1.11.534.jar /Applications/Fiji.app/jars/
+
