eb423bf210b484bc5268d7be5be37d0c621dfb03
Kimberly Meechan <kimberly.meechan@embl.de>
2020-12-03 16:29:14 +0000

    WIP: generate default table

--- src/main/java/de/embl/cba/mobie/ui/command/AddCurrentImageToProject.java
+++ src/main/java/de/embl/cba/mobie/ui/command/AddCurrentImageToProject.java
@@ -1,11 +1,21 @@
 package de.embl.cba.mobie.ui.command;
 
 import bdv.ij.ExportImagePlusAsN5PlugIn;
+import bdv.viewer.Source;
+import de.embl.cba.bdv.utils.BdvUtils;
+import de.embl.cba.bdv.utils.sources.LazySpimSource;
 import de.embl.cba.mobie.projects.ProjectsCreatorPanel;
 import de.embl.cba.tables.FileAndUrlUtils;
+import de.embl.cba.tables.Tables;
 import ij.IJ;
 import ij.ImagePlus;
 import net.imagej.ImageJ;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.roi.labeling.ImgLabeling;
+import net.imglib2.roi.labeling.LabelRegion;
+import net.imglib2.roi.labeling.LabelRegions;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.IntType;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
@@ -20,8 +30,14 @@ import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
+import javax.swing.*;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 
+import static de.embl.cba.morphometry.Utils.labelMapAsImgLabeling;
+import static de.embl.cba.morphometry.Utils.labelMapAsImgLabelingRobert;
 import static org.scijava.ItemVisibility.MESSAGE;
 
 @Plugin(type = Command.class, menuPath = "Plugins>MoBIE>Create>Add Current Image To MoBIE Project..." )
@@ -74,12 +90,77 @@ public class AddCurrentImageToProject implements Command {
 
             // update images.json
             projectsCreatorPanel.getProjectsCreator().addToImagesJson( imageName, imageType, chosenDataset );
+
+            // check it's a label image
+
+
+
+
+            // open xml as rai, at 0 resolution layer
+            // make labelRegions
+            // get centres and bbs
+            // make table, save table
         }
+
+
     }
 
     public static void main(final String... args) {
         final ImageJ ij = new ImageJ();
         ij.ui().showUI();
+
+        String[] columnNames = { "label_id", "anchor_x", "anchor_y",
+                "anchor_z", "bb_min_x", "bb_min_y", "bb_min_z", "bb_max_x",
+                "bb_max_y", "bb_max_z" };
+
+        final LazySpimSource labelsSource = new LazySpimSource( "labelImage", "C:\\Users\\meechan\\Documents\\temp\\mobie_test\\ruse\\data\\eldeer\\images\\local\\ahoy_label.xml" );
+        // has to already be as a labeling type
+        // warn needs to be integer, 0 counted as background
+        final RandomAccessibleInterval< IntType > rai = labelsSource.getNonVolatileSource( 0, 0);
+        ImgLabeling< Integer, IntType > imgLabeling = labelMapAsImgLabeling( rai );
+
+        LabelRegions labelRegions = new LabelRegions( imgLabeling );
+        Iterator<LabelRegion> labelRegionIterator = labelRegions.iterator();
+
+        ArrayList<Object[]> rows = new ArrayList<>();
+        // ArrayList<Integer> labelIds = new ArrayList<>();
+        // ArrayList<double[]> centres = new ArrayList<>();
+        // ArrayList<double[]> bbMins = new ArrayList<>();
+        // ArrayList<double[]> bbMaxs = new ArrayList<>();
+        while ( labelRegionIterator.hasNext() ) {
+            Object[] row = new Object[columnNames.length ];
+            LabelRegion labelRegion = labelRegionIterator.next();
+
+
+            double[] centre = new double[rai.numDimensions()];
+            labelRegion.getCenterOfMass().localize( centre );
+            double[] bbMin = new double[rai.numDimensions()];
+            double[] bbMax = new double[rai.numDimensions()];
+            labelRegion.realMin( bbMin );
+            labelRegion.realMax( bbMax );
+
+            row[0] =  labelRegion.getLabel();
+            row[1] = centre[0];
+            row[2] = centre[1];
+            row[3] = centre[2];
+            row[4] = bbMin[0];
+            row[5] = bbMin[1];
+            row[6] = bbMin[2];
+            row[7] = bbMax[0];
+            row[8] = bbMax[1];
+            row[9] = bbMax[2];
+
+            rows.add( row );
+
+        }
+
+        Object[][] rowArray = new Object[ rows.size() ] [ columnNames.length ];
+        rowArray = rows.toArray( rowArray );
+        // compensate for spacing
+
+        // make a Jtable
+        JTable table = new JTable( rowArray, columnNames);
+        Tables.saveTable( table, new File ("C:\\Users\\meechan\\Documents\\temp\\mobie_test\\ruse\\data\\eldeer\\images\\local\\test_output.csv"));
 
     }
 
