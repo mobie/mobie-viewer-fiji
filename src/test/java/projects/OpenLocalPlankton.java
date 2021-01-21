33fc1a0613d1e1bd2b223bf33261b67274b0990b
Christian Tischer <christian.tischer@embl.de>
2021-01-21 16:31:26 +0100

    Enable open from file system in [Open MoBIE Project...]

---
+++ src/test/java/projects/OpenLocalPlankton.java
@@ -0,0 +1,15 @@
+package projects;
+
+import de.embl.cba.mobie.ui.viewer.MoBIEViewer;
+import mpicbg.spim.data.XmlIoSpimData;
+import net.imagej.ImageJ;
+
+public class OpenLocalAutophagosomesEM
+{
+	public static void main( String[] args )
+	{
+		new ImageJ().ui().showUI();
+		//smb://schwab/schwab/schwab/MoBIE-plankton
+		final MoBIEViewer moBIEViewer = new MoBIEViewer("/g/kreshuk/pape/work/my_projects/autophagosoms-clem/data");
+	}
+}
