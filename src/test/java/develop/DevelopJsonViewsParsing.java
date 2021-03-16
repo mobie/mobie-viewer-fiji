97ea35ddf23410bf96e64d8729d508cc26500cda
Constantin Pape <c.pape@gmx.net>
2021-03-16 16:58:10 +0100

    Parsing hotfix

--- src/test/java/develop/DevelopJsonViewsParsing.java
+++ src/test/java/develop/DevelopJsonViewsParsing.java
@@ -5,6 +5,8 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DevelopJsonViewsParsing
 {
@@ -15,15 +17,37 @@ public class DevelopJsonViewsParsing
 
 	public static class TransformGroups
 	{
-		public ArrayList< Affine > transformGroups;
+		// public ArrayList< Affine > transformGroups;
+		public ArrayList<Map<String, Affine>> transformGroups;
 	}
 
 	public static void main( String[] args )
 	{
 		Gson gson = new Gson();
 
+		// forward serialization
+		/*
+		TransformGroups tg = new TransformGroups();
+
+		Map<String, Affine> trafo = new HashMap<String, Affine>();
+		Affine affine = new Affine();
+		double[] params = {1., 0., 0.};
+		affine.transform = params;
+		trafo.put("affine", affine);
+
+		ArrayList<Map<String, Affine>> trafos = new ArrayList<Map<String, Affine>>();
+		trafos.add(trafo);
+
+		tg.transformGroups = trafos;
+
+		String serialized = gson.toJson(tg);
+		System.out.println(serialized);
+		 */
+
+		// backward deserialization
 		Type type = new TypeToken< TransformGroups >() {}.getType();
-		TransformGroups transformGroups = gson.fromJson( json1(), type );
+		TransformGroups tg = gson.fromJson( json0(), type );
+		System.out.println(tg.transformGroups.get(0).get("affine").transform);
 	}
 
 	public static String json0()
