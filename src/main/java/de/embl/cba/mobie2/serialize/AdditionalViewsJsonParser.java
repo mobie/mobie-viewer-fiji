81350e81af1fae88ce723e036bec78393762a97a
Kimberly Meechan <kimberly.meechan@embl.de>
2021-05-06 16:51:16 +0100

    add serialisation for additional views files

---
+++ src/main/java/de/embl/cba/mobie2/serialize/AdditionalViewsJsonParser.java
@@ -0,0 +1,19 @@
+package de.embl.cba.mobie2.serialize;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import de.embl.cba.mobie2.AdditionalViews;
+import de.embl.cba.tables.FileAndUrlUtils;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+public class AdditionalViewsJsonParser {
+    public AdditionalViews getViews( String path ) throws IOException
+    {
+        final String s = FileAndUrlUtils.read( path );
+        Gson gson = new Gson();
+        Type type = new TypeToken< AdditionalViews >() {}.getType();
+        return gson.fromJson( s, type );
+    }
+}
