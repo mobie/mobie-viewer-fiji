93043ae819e2f55210510ed69f3ac4ddf272d700
Kimberly Meechan <kimberly.meechan@embl.de>
2021-06-01 09:21:07 +0100

    add latest changes from master

---
+++ src/main/java/de/embl/cba/mobie/n5/zarr/OMEZarrReader.java
@@ -0,0 +1,45 @@
+package de.embl.cba.mobie.n5.zarr;
+
+import com.google.gson.GsonBuilder;
+import ij.IJ;
+import mpicbg.spim.data.SpimData;
+import net.imglib2.util.Cast;
+
+import java.io.File;
+import java.io.IOException;
+
+public class OMEZarrReader
+{
+    private static boolean logChunkLoading;
+
+    private final String filePath;
+
+    public OMEZarrReader(String filePath)
+    {
+        this.filePath = filePath;
+    }
+
+    public static void setLogChunkLoading( boolean logChunkLoading )
+    {
+        OMEZarrReader.logChunkLoading = logChunkLoading;
+        if ( logChunkLoading ) IJ.run("Console");
+    }
+
+    public static SpimData openFile(String filePath) throws IOException
+    {
+        N5OMEZarrImageLoader.logChunkLoading = logChunkLoading;
+        OMEZarrReader omeZarrReader = new OMEZarrReader(filePath);
+        return omeZarrReader.readFile();
+    }
+
+    private SpimData readFile() throws IOException
+    {
+        N5OMEZarrImageLoader.logChunkLoading = logChunkLoading;
+        N5ZarrReader reader = new N5ZarrReader(this.filePath, new GsonBuilder());
+        N5OMEZarrImageLoader imageLoader = new N5OMEZarrImageLoader(reader);
+        return new SpimData(
+                new File(this.filePath),
+                Cast.unchecked( imageLoader.getSequenceDescription() ),
+                imageLoader.getViewRegistrations());
+    }
+}
