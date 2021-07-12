21c0dc16abe86fe9ab99663ad8e0d283b25a4fe6
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2021-07-12 15:04:14 +0300

    Renaming and WIP on OmeZarr opening

---
+++ src/test/java/projects/OpenLocalOmeZarr.java
@@ -0,0 +1,16 @@
+package projects;
+
+import de.embl.cba.mobie.MoBIE;
+import de.embl.cba.mobie.MoBIESettings;
+import de.embl.cba.mobie.source.ImageDataFormat;
+import net.imagej.ImageJ;
+
+import java.io.IOException;
+//WIP
+public class OpenLocalOmeZarr {
+    public static void main(String[] args) throws IOException {
+        final ImageJ imageJ = new ImageJ();
+        imageJ.ui().showUI();
+//        new MoBIE("/g/kreshuk/pape/Work/mobie/covid-if-project", MoBIESettings.settings().imageDataFormat(ImageDataFormat.OmeZarr));
+    }
+}
