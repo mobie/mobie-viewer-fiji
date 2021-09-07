85aa846454b35c460683e5043b5751c1c8cd8f34
Kimberly Meechan <kimberly.meechan@embl.de>
2021-09-07 16:54:17 +0100

    add more draft tests

--- src/test/java/de/embl/cba/mobie/projectcreator/DatasetsCreatorTest.java
+++ src/test/java/de/embl/cba/mobie/projectcreator/DatasetsCreatorTest.java
@@ -2,9 +2,11 @@ package de.embl.cba.mobie.projectcreator;
 
 import de.embl.cba.mobie.Project;
 import de.embl.cba.mobie.serialize.ProjectJsonParser;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,9 +16,10 @@ class DatasetsCreatorTest {
     private DatasetsCreator datasetsCreator;
 
     @org.junit.jupiter.api.BeforeEach
-    void setUp( @TempDir Path tempDir ) {
-        projectCreator = new ProjectCreator(tempDir);
+    void setUp( @TempDir Path tempDir ) throws IOException {
+        projectCreator = new ProjectCreator( tempDir.toFile() );
         datasetsCreator = projectCreator.getDatasetsCreator();
+        System.out.println(tempDir.toFile().getAbsolutePath());
     }
 
     @org.junit.jupiter.api.Test
@@ -31,7 +34,17 @@ class DatasetsCreatorTest {
     }
 
     @org.junit.jupiter.api.Test
-    void renameDataset() {
+    void renameDataset() throws IOException {
+        String oldDatasetName = "test";
+        String newDatasetName = "newName";
+        datasetsCreator.addDataset(oldDatasetName);
+        datasetsCreator.renameDataset(oldDatasetName, newDatasetName);
+
+        Project project = new ProjectJsonParser().parseProject( projectCreator.getProjectJson().getAbsolutePath() );
+        assertEquals(project.getDatasets().size(), 1);
+        assertEquals(project.getDatasets().get(0), newDatasetName);
+        assertTrue(new File(projectCreator.getDataLocation(), newDatasetName).exists());
+        assertFalse(new File(projectCreator.getDataLocation(), oldDatasetName).exists());
     }
 
     @org.junit.jupiter.api.Test
