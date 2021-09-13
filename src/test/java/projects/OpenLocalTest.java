1ebb39f1288b3f9ee7e3858df35b5284402d5886
Christian Tischer <christian.tischer@embl.de>
2021-09-13 09:35:40 +0200

    Remove scijvav.app from pom

--- src/test/java/projects/OpenLocalTest.java
+++ src/test/java/projects/OpenLocalTest.java
@@ -14,6 +14,6 @@ public class OpenLocalTest
 		final ImageJ imageJ = new ImageJ();
 		imageJ.ui().showUI();
 		new MoBIE("/g/kreshuk/pape/Work/mobie/covid-if-project/data", MoBIESettings.settings().imageDataFormat( ImageDataFormat.OmeZarr ).view( "default_with_tables" ) );
-//		new MoBIE("/g/emcf/hennies/for_constantin/mobie_no_table_test", MoBIESettings.settings().imageDataFormat( ImageDataFormat.BdvN5 ));
+//		new MoBIE("/g/emcf/pape/covid-if-project", MoBIESettings.settings().imageDataFormat( ImageDataFormat.BdvN5 ));
 	}
 }
