163ad93a4411acf541ec75a236bb9f1049e6b882
Christian Tischer <christian.tischer@embl.de>
2021-11-09 10:30:21 +0100

    Comment test

--- src/test/java/tests/TestRemoteZebrafish.java
+++ src/test/java/tests/TestRemoteZebrafish.java
@@ -1,13 +1,14 @@
 package tests;
 
 import net.imagej.ImageJ;
+import net.imagej.patcher.LegacyInjector;
 import org.embl.mobie.viewer.MoBIE;
 import org.embl.mobie.viewer.MoBIESettings;
 import org.embl.mobie.viewer.display.SegmentationSourceDisplay;
 import org.embl.mobie.viewer.source.ImageDataFormat;
 import org.embl.mobie.viewer.view.View;
 import org.embl.mobie.viewer.view.additionalviews.AdditionalViewsLoader;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Map;
@@ -20,7 +21,7 @@ public class TestRemoteZebrafish
 		//new TestRemoteZebrafish().testTransformedGridView();
 	}
 
-	@Test
+	//@Test
 	public void testSmallGridView() throws IOException
 	{
 		final ImageJ imageJ = new ImageJ();
