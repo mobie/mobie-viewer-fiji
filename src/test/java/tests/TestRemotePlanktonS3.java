8e78f3d14d2b124969d9d907b2cb2659c4309beb
Christian Tischer <christian.tischer@embl.de>
2021-11-27 20:25:24 +0100

    Add Plankton S3 test

---
+++ src/test/java/tests/TestRemotePlanktonS3.java
@@ -0,0 +1,30 @@
+package tests;
+
+import net.imagej.ImageJ;
+import org.embl.mobie.viewer.MoBIE;
+import org.embl.mobie.viewer.MoBIESettings;
+import org.embl.mobie.viewer.display.SegmentationSourceDisplay;
+import org.embl.mobie.viewer.source.ImageDataFormat;
+import org.embl.mobie.viewer.view.additionalviews.AdditionalViewsLoader;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class TestRemotePlanktonS3
+{
+	public static void main( String[] args ) throws IOException
+	{
+		new TestRemotePlanktonS3().run();
+	}
+
+	@Test
+	public void run() throws IOException
+	{
+		final ImageJ imageJ = new ImageJ();
+		imageJ.ui().showUI();
+
+		// This is special as it does not go via github but
+		// all information is on S3
+		final MoBIE moBIE = new MoBIE("https://s3.embl.de/plankton-fibsem", MoBIESettings.settings());
+	}
+}
