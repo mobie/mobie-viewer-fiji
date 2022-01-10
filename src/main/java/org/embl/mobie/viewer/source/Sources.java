40690fca2bf095ce3e7f374e1d87730cf8262823
Christian Tischer <christian.tischer@embl.de>
2022-01-10 12:05:08 +0100

    Copy classes from mobie.io

---
+++ src/main/java/org/embl/mobie/viewer/source/Sources.java
@@ -0,0 +1,73 @@
+package org.embl.mobie.viewer.source;
+
+import bdv.util.BdvFunctions;
+import bdv.util.BdvHandle;
+import bdv.util.BdvStackSource;
+import bdv.viewer.SourceAndConverter;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.converter.Converter;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.type.numeric.NumericType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Cast;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
+import org.embl.mobie.io.n5.source.LabelConverter;
+import org.embl.mobie.io.n5.source.LabelSource;
+import org.scijava.ui.behaviour.ClickBehaviour;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.util.Behaviours;
+
+import java.lang.reflect.Method;
+
+public abstract class Sources {
+    public static <R extends NumericType<R> & RealType<R>> SourceAndConverter<R> replaceConverter(SourceAndConverter<?> source, Converter<RealType<?>, ARGBType> converter) {
+        LabelSource<?> labelVolatileSource = new LabelSource(source.asVolatile().getSpimSource());
+        SourceAndConverter<?> volatileSourceAndConverter = new SourceAndConverter(labelVolatileSource, converter);
+        LabelSource<?> labelSource = new LabelSource(source.getSpimSource());
+        return new SourceAndConverter(labelSource, converter, volatileSourceAndConverter);
+    }
+
+    public static <R extends NumericType<R> & RealType<R>> BdvStackSource<?> showAsLabelMask(BdvStackSource<?> bdvStackSource) {
+        LabelConverter converter = new LabelConverter();
+        SourceAndConverter<R> sac = replaceConverter(bdvStackSource.getSources().get(0), converter);
+        BdvHandle bdvHandle = bdvStackSource.getBdvHandle();
+        bdvStackSource.removeFromBdv();
+
+        // access by reflection, which feels quite OK as this will be public in future versions of bdv anyway:
+        // https://github.com/bigdataviewer/bigdataviewer-vistools/commit/8cad3edac6c563dc2d22abf71345655afa7f49cc
+        try {
+            Method method = BdvFunctions.class.getDeclaredMethod("addSpimDataSource", BdvHandle.class, SourceAndConverter.class, int.class);
+            method.setAccessible(true);
+            BdvStackSource<?> newBdvStackSource = (BdvStackSource<?>) method.invoke("addSpimDataSource", bdvHandle, sac, 1);
+
+            Behaviours behaviours = new Behaviours(new InputTriggerConfig());
+            behaviours.install(bdvHandle.getTriggerbindings(), "label source " + sac.getSpimSource().getName());
+            behaviours.behaviour((ClickBehaviour) (x, y) ->
+                            new Thread(() -> {
+                                converter.getColoringModel().incRandomSeed();
+                                bdvHandle.getViewerPanel().requestRepaint();
+                            }).start(),
+                    "shuffle random colors " + sac.getSpimSource().getName(),
+                    "ctrl L");
+
+            return newBdvStackSource;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * TODO: add time
+     *
+     * @param bdvStackSource
+     * @param level
+     */
+    public static void viewAsHyperstack(BdvStackSource<?> bdvStackSource, int level) {
+        RandomAccessibleInterval<?> rai = bdvStackSource.getSources().get(0).getSpimSource().getSource(0, level);
+        IntervalView<?> permute = Views.permute(Views.addDimension(rai, 0, 0), 2, 3);
+        ImageJFunctions.wrap(Cast.unchecked(permute), "em").show();
+    }
+}
---
+++ src/main/java/org/embl/mobie/viewer/view/OMEZarrViewer.java
@@ -0,0 +1,55 @@
+package org.embl.mobie.viewer.view;
+
+import bdv.util.BdvFunctions;
+import bdv.util.BdvOptions;
+import bdv.util.BdvStackSource;
+import mpicbg.spim.data.SpimData;
+import mpicbg.spim.data.sequence.ViewSetup;
+import net.imglib2.Dimensions;
+import org.embl.mobie.io.n5.source.Sources;
+
+import java.util.List;
+
+public class OMEZarrViewer
+{
+    private final SpimData spimData;
+
+    public OMEZarrViewer( SpimData spimData) {
+        this.spimData = spimData;
+    }
+
+    public void show() {
+        final List<ViewSetup> viewSetups = spimData.getSequenceDescription().getViewSetupsOrdered();
+
+        BdvOptions bdvOptions = getBdvOptions(viewSetups);
+
+        List<BdvStackSource<?>> sources = BdvFunctions.show(spimData, bdvOptions);
+
+        for (int i = 0; i < viewSetups.size(); i++) {
+            final String name = viewSetups.get(i).getChannel().getName();
+            if (name.contains("labels"))
+                Sources.showAsLabelMask(sources.get(i));
+        }
+    }
+
+    public BdvOptions getBdvOptions(List<ViewSetup> viewSetups) {
+        boolean is2D = is2D(viewSetups);
+        BdvOptions bdvOptions = new BdvOptions();
+
+        if (is2D) {
+            bdvOptions = bdvOptions.is2D();
+        }
+        return bdvOptions;
+    }
+
+    public boolean is2D(List<ViewSetup> viewSetups) {
+        for (ViewSetup viewSetup : viewSetups) {
+            final Dimensions size = viewSetup.getSize();
+            if (size.dimensionsAsLongArray().length > 2) {
+                if (size.dimension(2) > 1)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
