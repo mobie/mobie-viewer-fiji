31207fe29accfd7cfc8f4a9b74bda21e96ccbfe3
Christian Tischer <christian.tischer@embl.de>
2022-05-16 17:31:52 +0200

    Fixes #709

--- src/main/java/org/embl/mobie/viewer/annotate/DefaultAnnotatedMaskTableRow.java
+++ src/main/java/org/embl/mobie/viewer/annotate/DefaultAnnotatedMaskTableRow.java
@@ -28,12 +28,9 @@ public class DefaultAnnotatedMaskTableRow extends AbstractTableRow implements An
 
 		// set cells
 		this.cells = new LinkedHashMap<>();
-		final List< String > columnNames = new ArrayList<>( columns.keySet() );
-		//Collections.sort( columnNames ); // TODO: why do we sort here?
-		for ( String column : columnNames )
-		{
+		for ( String column : columns.keySet() )
 			cells.put( column, columns.get( column ).get( rowIndex ) );
-		}
+
 	}
 
 	@Override
