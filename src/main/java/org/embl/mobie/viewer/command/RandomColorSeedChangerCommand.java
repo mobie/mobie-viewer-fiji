f646f341240e13fd36456bac222d39dcabec28ee
Christian Tischer <christian.tischer@embl.de>
2022-05-17 16:07:55 +0200

    Implement getRandomSeed() method for CategoryColoringModel

--- src/main/java/org/embl/mobie/viewer/command/RandomColorSeedChangerCommand.java
+++ src/main/java/org/embl/mobie/viewer/command/RandomColorSeedChangerCommand.java
@@ -1,18 +1,24 @@
 package org.embl.mobie.viewer.command;
 
 import bdv.viewer.SourceAndConverter;
+import de.embl.cba.tables.color.CategoryColoringModel;
+import de.embl.cba.tables.color.ColoringModel;
+import ij.IJ;
 import org.embl.mobie.viewer.color.LabelConverter;
+import org.scijava.Initializable;
+import org.scijava.command.DynamicCommand;
+import org.scijava.module.MutableModuleItem;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import org.scijava.widget.Button;
 import sc.fiji.bdvpg.scijava.ScijavaBdvDefaults;
 import sc.fiji.bdvpg.scijava.command.BdvPlaygroundActionCommand;
 
 @Plugin(type = BdvPlaygroundActionCommand.class, menuPath = CommandConstants.CONTEXT_MENU_ITEMS_ROOT + "Display>Change Random Coloring Seed [ Ctrl L ]")
 public class RandomColorSeedChangerCommand implements BdvPlaygroundActionCommand
 {
-	// TODO: Maybe implement a setter (the Categorical coloring models have the method for this already)
-	//@Parameter( label = "Random color seed" )
-	//int seed = 42;
+	@Parameter( label = "Increment random color seed [ Ctrl L ]", callback = "incrementRandomColorSeed" )
+	Button button;
 
 	@Parameter
 	SourceAndConverter[] sourceAndConverters;
@@ -31,7 +37,12 @@ public class RandomColorSeedChangerCommand implements BdvPlaygroundActionCommand
 			{
 				final LabelConverter< ? > converter = ( LabelConverter< ? > ) sourceAndConverter.getConverter();
 
-				converter.getColoringModel().incrementRandomColorSeed();
+				final ColoringModel< ? > coloringModel = converter.getColoringModel().getWrappedColoringModel();
+				if ( coloringModel instanceof CategoryColoringModel )
+				{
+					int randomSeed = ( ( CategoryColoringModel< ? > ) coloringModel ).getRandomSeed();
+					( ( CategoryColoringModel<?> ) coloringModel ).setRandomSeed( ++randomSeed );
+				}
 			}
 		}
 	}
