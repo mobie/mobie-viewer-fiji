b88ef83ea7b8e97c8f5274451c01fd6fbcb5a9f7
Christian Tischer <christian.tischer@embl.de>
2022-05-18 13:04:41 +0200

    defaultLocation is new PositionViewerTransform( new double[]{0,0,0}, 0 )

--- src/main/java/org/embl/mobie/viewer/Dataset.java
+++ src/main/java/org/embl/mobie/viewer/Dataset.java
@@ -1,6 +1,7 @@
 package org.embl.mobie.viewer;
 
 import org.embl.mobie.viewer.source.SourceSupplier;
+import org.embl.mobie.viewer.transform.PositionViewerTransform;
 import org.embl.mobie.viewer.transform.ViewerTransform;
 import org.embl.mobie.viewer.view.View;
 
@@ -9,7 +10,7 @@ import java.util.Map;
 public class Dataset
 {
 	public boolean is2D = false;
-	public ViewerTransform defaultLocation;
+	public ViewerTransform defaultLocation = new PositionViewerTransform( new double[]{0,0,0}, 0 );
 	public int timepoints = 1;
 	public Map< String, SourceSupplier> sources;
 	public Map< String, View> views;
