a6fb9a5ee2c67c1d952b0cf14f7435e81e0bfc56
Christian Tischer <christian.tischer@embl.de>
2022-06-10 20:12:23 +0200

    Change OpenRemoteCovidIF view

--- src/test/java/projects/OpenRemoteCovidIF.java
+++ src/test/java/projects/OpenRemoteCovidIF.java
@@ -46,6 +46,7 @@ public class OpenRemoteCovidIF
 		final ImageJ imageJ = new ImageJ();
 		imageJ.ui().showUI();
 
-		final MoBIE moBIE = new MoBIE( "https://github.com/mobie/covid-if-project", MoBIESettings.settings().view( "merge-grid-no-tables" ) );
+		final MoBIE moBIE = new MoBIE( "https://github.com/mobie/covid-if-project", MoBIESettings.settings().view( "default" ) );
+		// "merge-grid-no-tables"
 	}
 }
