03d9f155edf3b26ef90b3831337be0419905efbd
Christian Tischer <christian.tischer@embl.de>
2022-09-13 17:03:47 +0200

    Update pom

--- src/test/java/develop/DemoOpenOMEZARRFromS3WithCredentialsCommand.java
+++ src/test/java/develop/DemoOpenOMEZARRFromS3WithCredentialsCommand.java
@@ -28,12 +28,14 @@
  */
 package develop;
 
+import net.imagej.ImageJ;
 import org.embl.mobie.viewer.command.OpenOMEZARRFromS3WithCredentialsCommand;
 
 public class DemoOpenOMEZARRFromS3WithCredentialsCommand
 {
 	public static void main( String[] args )
 	{
+		final ImageJ imageJ = new ImageJ();
 		final OpenOMEZARRFromS3WithCredentialsCommand command = new OpenOMEZARRFromS3WithCredentialsCommand();
 		command.s3URL = "https://s3.embl.de/comulis/elena/images/bdv-ome-zarr/SXAA03648.ome.zarr";
 		command.s3AccessKey = "UYP3FNN3V5F0P86DR2O3";
