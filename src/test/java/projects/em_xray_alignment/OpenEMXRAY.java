d72930db778b83a1125e4ba678d84949a54030c7
Christian Tischer <christian.tischer@embl.de>
2023-12-18 17:58:55 +0100

    IncuCyteRaw reading from S3 working

--- src/test/java/projects/em_xray_alignment/OpenEMXRAY.java
+++ src/test/java/projects/em_xray_alignment/OpenEMXRAY.java
@@ -1,13 +1,8 @@
 package projects.em_xray_alignment;
 
-import de.embl.cba.transforms.utils.Transforms;
 import net.imagej.ImageJ;
-import net.imglib2.util.LinAlgHelpers;
 import org.embl.mobie.MoBIE;
-import org.embl.mobie.MoBIESettings;
 import org.embl.mobie.io.OpenerLogging;
-import org.embl.mobie.lib.io.DataFormats;
-import org.embl.mobie.lib.transform.TransformHelper;
 
 import java.io.IOException;
 
