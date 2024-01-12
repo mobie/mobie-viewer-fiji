10520adbdeefbf30c75caf708e5710140b2ae03a
Christian Tischer <christian.tischer@embl.de>
2024-01-12 17:40:15 +0100

    Add some comments

--- src/test/java/develop/hcs/HCSMoritz.java
+++ src/test/java/develop/hcs/HCSMoritz.java
@@ -11,6 +11,15 @@ public class HCSMoritz
 {
     public static void main( String[] args ) throws SpimDataException, IOException
     {
+        // QUESTIONS:
+        // - What is the layout of the wells (numbers => position)
+        // - What to do with the z positions?
+        // - Is python preprocessing OK, for filtering, segmentation and object counting?
+        //    - output: filtered well images, well segmentations, well table with paths
+        // - What are the time lines? I cannot really commit to delivery dates...
+        // TODO:
+        // - Replace the name of the HCSPattern: Moritz -> real name
+
         new ImageJ().ui().showUI();
         new MoBIE( "/Users/tischer/Desktop/moritz/CQ1_testfiles",
                 new MoBIESettings(),
