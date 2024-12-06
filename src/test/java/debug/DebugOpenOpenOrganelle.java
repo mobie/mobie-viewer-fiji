4d6c8f3e5067ab4643e7eb06001ef1d5629ba0e1
Christian Tischer <christian.tischer@embl.de>
2024-12-06 16:48:11 +0100

    Merge latest main and add DebugOpenOpenOrganelle

--- src/test/java/debug/DebugOpenOpenOrganelle.java
+++ src/test/java/debug/DebugOpenOpenOrganelle.java
@@ -1,11 +1,15 @@
 package debug;
 
+import net.imagej.ImageJ;
 import org.embl.mobie.command.open.OpenImageAndLabelsCommand;
+import org.embl.mobie.lib.image.Image;
 
 public class DebugOpenOpenOrganelle
 {
     public static void main( String[] args )
     {
+        new ImageJ().ui().showUI();
+
         OpenImageAndLabelsCommand command = new OpenImageAndLabelsCommand();
         command.image = "s3://janelia-cosem-datasets/jrc_hela-3/jrc_hela-3.n5/em/fibsem-uint16";
         command.run();
