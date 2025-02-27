5d4002afcebee0d5f5152b2882b2b48e0a294b8b
Christian Tischer <christian.tischer@embl.de>
2025-02-27 11:48:33 +0100

    Add debug for SpotsRenderingOnWindows

---
+++ src/test/java/issues/SpotsRenderingOnWindows.java
@@ -0,0 +1,21 @@
+package issues;
+
+import net.imagej.ImageJ;
+import org.embl.mobie.command.open.OpenCollectionTableCommand;
+import org.embl.mobie.lib.bdv.BdvViewingMode;
+
+import java.io.File;
+
+public class SpotsRenderingOnWindows
+{
+    public static void main( String[] args )
+    {
+        new ImageJ().ui().showUI();
+
+        OpenCollectionTableCommand command = new OpenCollectionTableCommand();
+        command.table = new File("/Users/tischer/Desktop/mobie-bug/collection.txt");
+        command.dataRoot = OpenCollectionTableCommand.DataRoot.UseTableFolder;
+        command.bdvViewingMode = BdvViewingMode.TwoDimensional;
+        command.run();
+    }
+}
